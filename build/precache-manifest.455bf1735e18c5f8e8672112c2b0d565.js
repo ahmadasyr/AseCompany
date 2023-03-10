45167c742b1acedbaa8460f480302ae9296656ca
ahmadasyr <ahmad.aboud2002@gmail.com>
2023-03-10 04:15:06 +0300

    build

---
+++ build/precache-manifest.455bf1735e18c5f8e8672112c2b0d565.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c303c6aea86fa60dd13b17e8d1ad9f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bc59763d1d130307ce",
+    "url": "/static/css/2.8a8b28d0.chunk.css"
+  },
+  {
+    "revision": "78c27797bdcc629d4669",
+    "url": "/static/css/main.42a69556.chunk.css"
+  },
+  {
+    "revision": "75bc59763d1d130307ce",
+    "url": "/static/js/2.be051a90.chunk.js"
+  },
+  {
+    "revision": "7ae7c62981dff5b0bbcdc5a4d16860d7",
+    "url": "/static/js/2.be051a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c27797bdcc629d4669",
+    "url": "/static/js/main.28908046.chunk.js"
+  },
+  {
+    "revision": "ea5b2784fc0f2bd01dcd",
+    "url": "/static/js/runtime-main.c9b68fb7.js"
+  }
+]);

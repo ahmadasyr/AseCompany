ab2862458870f21287f1607325af8063bea5803a
ahmadasyr <ahmad.aboud2002@gmail.com>
2023-03-11 05:22:56 +0300

    fixed nav logo

---
+++ build/precache-manifest.b33558c459b9bc0738a381c1f47c658a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6926954b48c53079c711cb183c7fd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bc59763d1d130307ce",
+    "url": "/static/css/2.8a8b28d0.chunk.css"
+  },
+  {
+    "revision": "d8528463e4cc3de6da86",
+    "url": "/static/css/main.42a69556.chunk.css"
+  },
+  {
+    "revision": "75bc59763d1d130307ce",
+    "url": "/static/js/2.be051a90.chunk.js"
+  },
+  {
+    "revision": "7ae7c62981dff5b0bbcdc5a4d16860d7",
+    "url": "/static/js/2.be051a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8528463e4cc3de6da86",
+    "url": "/static/js/main.230a3433.chunk.js"
+  },
+  {
+    "revision": "ea5b2784fc0f2bd01dcd",
+    "url": "/static/js/runtime-main.c9b68fb7.js"
+  }
+]);
